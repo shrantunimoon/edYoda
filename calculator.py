babec80efb1735a241ad8dc96264f0e5ca5b8938
Shrantuni Moon <113660823+shrantunimoon@users.noreply.github.com>
2025-06-15 22:55:33 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,202 @@
+{
+ "cells": [
+  {
+   "cell_type": "raw",
+   "id": "f6ee6a2b-a144-4f06-aa08-a26df58d17f3",
+   "metadata": {},
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "2cc55812-f473-4260-a3c5-a020243320a8",
+   "metadata": {
+    "scrolled": true
+   },
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter first number 12\n",
+      "enter second number 13\n",
+      "what operation do you want to perform : +,-,*,/ +\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "12+13=25\n"
+     ]
+    }
+   ],
+   "source": [
+    "''' A simple calculator to add,substract,multiply and divide'''\n",
+    "first=int(input('Enter first number')) \n",
+    "second=int(input('enter second number')) \n",
+    "op=input('what operation do you want to perform : +,-,*,/')  \n",
+    "if op=='+':\n",
+    "    print(f'{first}+{second}={first+second}') \n",
+    "if op=='-':\n",
+    "    print(f'{first}+{second}={first-second}')\n",
+    "if op=='*':\n",
+    "    print(f'{first}+{second}={first*second}') \n",
+    "if op=='/' :\n",
+    "    if second!=0:\n",
+    "        print(f'{first}+{second}={first/second}') \n",
+    "    else:\n",
+    "        print('cannot divide by 0')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "b833d5a6-0486-4813-824d-28e06d1499d0",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d7f38dff-b1bc-4861-b89f-13fa97494120",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "65b9f101-8b27-4e43-b1ec-25425eeffc2d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "da5b848e-fda2-4af7-8c20-4938fc4ecd47",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "5b58195d-3a23-4403-a63e-1809c78fd249",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "f24af282-a223-4b5c-8722-b03bd8f3cc0f",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "e602d724-c603-408d-b603-4f9481444504",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "e16b31a5-4b21-41db-8d9d-0a5619b0b262",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "24f7fa6a-45b8-4e44-b6e0-477dccad6247",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "15f07fc7-d76b-439d-b485-d6e37d1dcccf",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "664658d8-1201-49b9-821d-8567e11d5d45",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "670c20d4-6318-4834-ae85-3c6469e31ad6",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "e654f428-d7e4-469f-b33b-04cf723e4ed5",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "84212b6c-5d95-4b74-872c-782b57bed9cb",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "83848926-776c-44c5-bba7-ef965a2d79c0",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8a42d2ee-0910-43d1-a628-2dc6de98234b",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
