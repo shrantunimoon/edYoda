d25a36e1dff64d2355ad5ddab91442747f56d8e1
Shrantuni Moon <113660823+shrantunimoon@users.noreply.github.com>
2025-06-16 00:20:49 -0700

    Add files via upload

---
+++ temp_cal.py
@@ -0,0 +1,165 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "f2398307-7a1d-470d-a1f9-9e790b5c3e52",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "give temperature 32\n",
+      "the temp is in what format celsius or fahrenheit,\"C\",\"F\" c\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "your temp in fahrenheit : 89.6\n"
+     ]
+    }
+   ],
+   "source": [
+    "''getting temperatue in one format and converting it to another'''\n",
+    "\n",
+    "temp=int(input('give temperature')) \n",
+    "type=(input('the temp is in what format celsius or fahrenheit,\"C\",\"F\"')).upper()\n",
+    "if type=='C':\n",
+    "    print(f'your temp in fahrenheit : {(temp*(9/5))+32}') \n",
+    "elif type=='F':\n",
+    "    print(f'your temp in fahrenheit : {(temp-32)*(5/9)}') \n",
+    "else:\n",
+    "    print('invalid input') \n",
+    "    "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ed83f961-532a-4d9a-93f6-67e80b29a2bb",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "1d5dfb15-ac34-4e6b-98a1-3357bfe0dd8c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d4cc243c-ea97-4169-9cbb-e808b5e6242b",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "c6b06ed9-dce7-4fdd-817d-9d06f037bc33",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "fcd7d130-4101-4cb2-9be8-073d6f82a4d4",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "1b2ec67d-f488-4d26-85ad-297167170ef5",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "45259747-4c38-45bd-a554-35964db2e5ba",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d53bff45-e9c1-4f46-8b0e-cd72517e4c97",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "f4c8df7f-6925-47bb-a404-0ac4f782a5b8",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "68aed4e8-7b6a-45c6-ad8d-264170d67f2e",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "3b055bbb-5e3e-4a9a-adb0-1937ebcda643",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "0476f686-3c56-4622-8066-d49977ee7691",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "50b72120-65ee-4725-a767-2a68fc50ea0d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
