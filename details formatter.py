42bb77cea6fa3d93fc8cefdee9ed6b4b13084844
Shrantuni Moon <113660823+shrantunimoon@users.noreply.github.com>
2025-06-16 21:42:10 -0700

    Add files via upload

---
+++ details formatter.py
@@ -0,0 +1,177 @@
+{
+ "cells": [
+  {
+   "cell_type": "raw",
+   "id": "b5a1875e-2a42-489c-8ebe-aafac396d08c",
+   "metadata": {},
+   "source": [
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "id": "ef97f83a-ab52-4354-ad39-d212636db781",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "first_name shma\n",
+      "last_name shabbir\n",
+      "age 34\n",
+      "height 4.5\n",
+      "weight 50\n",
+      "i have insurance,say insured or uninsured insured\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "name :Shma Shabbir,\n",
+      "age:34 years,\n",
+      "weight:50Kg and22.0lbs,\n",
+      "insureance status:insured\n"
+     ]
+    }
+   ],
+   "source": [
+    "first_name=(input('first_name')).capitalize() \n",
+    "last_name=(input('last_name')).capitalize() \n",
+    "age=int(input('age')) \n",
+    "height=float(input('height')) \n",
+    "weight=int(input('weight'))  \n",
+    "insurance=(input('i have insurance,say insured or uninsured')) \n",
+    "\n",
+    "print(f'name :{first_name+' '+last_name},\\nage:{age} years,\\nweight:{weight}Kg and{weight//2.20462}lbs,\\ninsureance status:{insurance}')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "397ed523-d56c-45bd-9436-89abe4344b58",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "38dd1bde-7d6c-4ed2-82fa-c93717f1f56f",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "6794ee61-c71c-4bd1-8f0f-1fabadaa2ffd",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d341711a-dc46-4075-b0f5-f6a12104e787",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "2252a3b4-be72-4582-951f-51b809af27e7",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "df90d2dc-a85e-45b8-92c2-a6c26106bbe2",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "c9ff2e45-ef3c-4b70-8b27-5c2e6dfcd269",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ce235000-b380-4b20-b785-5d48552e602a",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d7a81cbc-c0cf-482f-b1aa-bd3a4135d179",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "09bc43fa-094d-4317-9059-b1e21062839a",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "b2ae081a-2f23-46da-87c5-1d693e6c514c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "936562b3-f86c-4a76-9672-ca2feca969e8",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "5000cfa8-1231-49e0-9b13-14959ee8844c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
