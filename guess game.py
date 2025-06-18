bd4b9303b90ac29febaad56286809dc976b54c44
Shrantuni Moon <113660823+shrantunimoon@users.noreply.github.com>
2025-06-17 23:23:40 -0700

    Add files via upload

---
+++ guess game.py
@@ -0,0 +1,205 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "id": "5a53e460-8973-4faf-8411-aad14a050596",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "do you want to continue,quit or not quit\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "thank you for playing\n"
+     ]
+    }
+   ],
+   "source": [
+    "import random\n",
+    "\n",
+    "     \n",
+    "dec=input('do you want to continue,quit or not') \n",
+    "if dec.lower()!=\"quit\": \n",
+    "    i=1\n",
+    "    while i<=7:\n",
+    "        inp=int(input('provide your input') )\n",
+    "        sys=random.randint(1,100) \n",
+    "        if inp==sys:\n",
+    "            print('winner')\n",
+    "            break \n",
+    "        else:\n",
+    "            print('wrong input')\n",
+    "            i+=1 \n",
+    "else:\n",
+    "    print('thank you for playing')\n",
+    "    \n",
+    "    \n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "35fb9f5a-5ab0-4bbe-ad2c-bcf34b95ff0d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d20315ca-7982-4712-b7b7-c7507aa674d4",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "786f02d5-14cd-40e9-bdab-d6d58d448c9d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "62f4a635-06cc-4540-a57d-c49428e87e9c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "4b56f8de-3a4a-4d78-b467-55fb7ed3c41a",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "f8be554e-03a1-4c75-80e6-3097454a74fc",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "57ce9f6b-451a-4575-9f10-d775436fc525",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ca8718e5-c957-41d2-890f-72228b62b7ed",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "0a0bc20d-791b-4bd8-a49a-36aa54d7ba91",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "335449f1-f7d1-4574-bb4e-5a999e0db2f6",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "89ac3dd1-2a18-4f6b-bb8d-955be5f7bcce",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ac7a237b-d269-4be5-8a91-d79102c5ed41",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "73557412-c1d4-4c1d-80a9-8eaa8264740d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "7e5c41a9-6c58-49b5-93e9-b8f62cf149dd",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "7d255c44-377a-4cb4-8a8b-1be3720c1808",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "a4c8a164-2d3b-4527-8147-517fa5974a3f",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "fcadeb54-27c4-4848-90db-9545b8057df2",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
